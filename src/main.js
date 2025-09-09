baa1d9d73be57364ec2145fd2fc7c2a4232f84f1
andrii-bondarenko-dev <bondarenkoandriy000@gmail.com>
2025-09-09 15:19:59 +0300

    feat: banner mobile done

--- src/main.js
+++ src/main.js
@@ -15,10 +15,13 @@ const getLoremImage = () => {
 const burgerBtn = document.querySelector('#burger');
 const mobileContainer = document.querySelector('#mobile-container');
 
+const body = document.querySelector('body')
+
 burgerBtn.addEventListener('click', () => {
   mobileContainer.classList.toggle('hidden')
   burgerBtn.classList.toggle('after:bg-purple')
   burgerBtn.classList.toggle('before:bg-purple')
+  body.classList.toggle('overflow-hidden')
   
 })
 
